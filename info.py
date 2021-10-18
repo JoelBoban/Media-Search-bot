8991002966f30988ab879de0b69b00a2b8c8d7e3
JoelBoban14 <92171948+JoelBoban14@users.noreply.github.com>
2021-10-18 06:26:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,13 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+**𝐘𝐨..𝐘𝐨..Good evening 𝐉𝐨𝐞𝐥 #𝐃𝐂 1 🙋, I'm Powerful Auto-Filter Bot You Can Use Me As A Auto-filter Bot In Your Group ..
+
+Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...🤓
+
+⚠️More Help Check Help Button Below**
+
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ  
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
