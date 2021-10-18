8c00bd8527ee6946984f3d4b045c84f711e2153c
JoelBoban14 <92171948+JoelBoban14@users.noreply.github.com>
2021-10-18 06:19:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -59,14 +59,14 @@ async def save_poster(imdb_id, title, year, url):
             poster=url
         )
     except ValidationError:
-        logger.exception('Error occurred while saving poster in database')
+        logger.exception('⚠️ Eʀʀᴏʀ ᴏᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ sᴀᴠɪɴɢ ᴛᴏ ᴅᴀᴛᴀʙᴀsᴇ')
     else:
         try:
             await data.commit()
         except DuplicateKeyError:
-            logger.warning("already saved in database")
+            logger.warning("Aʟʀᴇᴀᴅʏ sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ")
         else:
-            logger.info("Poster is saved in database")
+            logger.info("Pᴏsᴛᴇʀ ɪs sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ")
 
 async def save_file(media):
     """Save file in database"""
@@ -90,9 +90,9 @@ async def save_file(media):
         try:
             await file.commit()
         except DuplicateKeyError:
-            logger.warning(media.file_name + " is already saved in database")
+            logger.warning(media.file_name + " ɪs ᴀʟʀᴇᴀᴅʏ sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ")
         else:
-            logger.info(media.file_name + " is saved in database")
+            logger.info(media.file_name + " ɪs sᴀᴠᴇᴅ ɪɴ ᴅᴀᴛᴀʙᴀsᴇ")
 
 
 async def get_search_results(query, file_type=None, max_results=10, offset=0):
